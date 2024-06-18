7b3d0bf0afdd360640adbc8fa0ba6ad1db02210b
michaelperryjnr <michaelperryt97@gmail.com>
2024-06-18 23:28:44 +0000

    Default exports

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+import { beats, burgerKing, profile } from "./images";
+
+import { appleIcon, googleIcon, facebookIcon } from "./icons";
+
+export { appleIcon, googleIcon, facebookIcon, beats, burgerKing, profile };
