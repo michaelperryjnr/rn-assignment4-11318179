1b5e1422eec3058c5e8aab882f73b0a7ee4abffb
michaelperryjnr <michaelperryt97@gmail.com>
2024-06-18 23:28:23 +0000

    Added images

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import burgerKing from "../assets/images/burger-king.png";
+import profile from "../assets/images/profile.png";
+import beats from "../assets/images/beats.png";
+
+export { burgerKing, profile, beats };
