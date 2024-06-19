b4590c51c5532f19cb6afa3e090450f3d0571fd9
michaelperryjnr <michaelperryt97@gmail.com>
2024-06-19 17:10:49 +0000

    Created job offer component

---
+++ components/jobOffer.jsx
@@ -0,0 +1,20 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const JobOffer = ({ image, position, company, salary, location }) => {
+  return (
+    <View className="flex-row justify-start items-start px-1 py-4  rounded-[15px] mb-4 bg-white">
+      <Image source={image} className="w-12 h-12" />
+      <View className="flex flex-col items-center justify-start">
+        <Text className="text-black font-bold text-[15px]">{position}</Text>
+        <Text className="text-gray-400 text-[15px]">{company}</Text>
+      </View>
+      <View className="flex flex-col items-end justify-end">
+        <Text className="text-black font-bold text-[15px] text-right">{salary}</Text>
+        <Text className="text-gray-400 text-[15px]">{location}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default JobOffer;
